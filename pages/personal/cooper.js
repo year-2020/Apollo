85398be616a8833631c6546e2d1f9364f7e1a14d
dengyongliang <dyliang1985@hotmail.com>
2020-04-02 23:47:16 +0800

    20200402

--- pages/personal/cooper.js
+++ pages/personal/cooper.js
@@ -7,6 +7,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    title: '合作登记',
     modalShow: false,
     organizationName: '',
     name: '',
@@ -55,7 +56,9 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    wx.setNavigationBarTitle({
+      title: this.data.title,
+    })
   },
 
   /**
